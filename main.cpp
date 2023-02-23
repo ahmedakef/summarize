db531c4ab4c889415f5c739853cd146824efe55c
ahmedakef <aemed.akef.1@gmail.com>
2023-02-23 15:00:29 +0200

    handle reading from file

--- main.cpp
+++ main.cpp
@@ -2,32 +2,36 @@
 #include <sstream>
 #include <vector>
 #include <thread>
-#include "modules/summarizer.h"
-#include "utils/utils.h"
+#include "modules/summarizer.hpp"
+#include "utils/utils.hpp"
+#include <fcntl.h>
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 
 using namespace std;
 bool continue_reading = true;
 
-void handle_printing(Summarizer *summarizer)
+void handle_printing(Summarizer *summarizer, int delay)
 {
     printElements(vector<string>{"Average", "Min", "Median", "p95", "p99", "Max", "\n"});
     while (true)
     {
         cout << "\r";
         summarizer->print_summery();
-        this_thread::sleep_for(chrono::seconds(1));
-        if (continue_reading == false)
+        this_thread::sleep_for(chrono::seconds(delay));
+        if (continue_reading == false){
+            cout<<endl;
             break;
+        }
     }
 }
 
-int main()
-{
+void start(int delay) {
     int number;
     string line;
     Summarizer summarizer;
 
-    thread timer(handle_printing, &summarizer);
+    thread timer(handle_printing, &summarizer, delay);
 
     while (continue_reading)
     {
@@ -46,5 +50,51 @@ int main()
     }
 
     timer.join();
+}
+
+int main(int ac, char* av[])
+{
+    try {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help,h", "produce help message")
+            ("file,f", po::value<string>(), "read input from a file")
+            ("delay,d", po::value<int>(), "delay time between re-calculating")
+        ;
+
+        po::variables_map vm;        
+        po::store(po::parse_command_line(ac, av, desc), vm);
+        po::notify(vm); 
+
+        if (vm.count("help")) {
+            cout << desc << "\n";
+            return 0;
+        }
+
+        if (vm.count("file")) {
+            string file_name = vm["file"].as<string>();
+            // close the stdin so when we open the file it take the lowest
+            // available file descriptor which is 0
+            close(0);
+            int fd = open(file_name.c_str(), 0);
+            if (fd < 0) {
+                cout<<"cat: cannot open "<<file_name<<endl;
+                exit(1);
+            }
+            
+        }
+        int delay = 1;
+        if (vm.count("delay")) {
+            delay = vm["delay"].as<int>();
+        }
+        start(delay);
+    } catch(exception& e) {
+        cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        cerr << "Exception of unknown type!\n";
+    }
+    
     return 0;
 }
--- makefile
+++ makefile
@@ -1,6 +1,7 @@
 all:
-	g++ -I boost_1_81_0 \
+	c++ \
 	-std=c++11 \
 	-pthread \
 	-o summarizer \
-	main.cpp utils/utils.cpp modules/summarizer.cpp
+	main.cpp utils/utils.cpp modules/summarizer.cpp \
+	-lboost_program_options
