cda01055be4af74c9bfef2d26d8c530be575cef5
ahmedakef <aemed.akef.1@gmail.com>
2023-03-07 20:34:05 +0200

    add the generator source code

---
+++ gernerator.cpp
@@ -0,0 +1,19 @@
+// compile using: g++ gernerator.cpp -o generator
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+
+    int numbers_count = 2000000000;
+    while (numbers_count--)
+    {
+        // cout<<"the request took "<<rand() % 1000 << " seconds to complete"<<endl;
+        cout << rand() % 1000 << endl;
+        this_thread::sleep_for(400ms);
+    }
+
+    return 0;
+}
